2910f47f26aaa4962638ee9293cbda8265859097
itsmeaiswarya <69300687+itsmeaiswarya@users.noreply.github.com>
2022-02-23 20:48:39 +0530

    Create failed.php

---
+++ BrokenAuthentication/failed.php
@@ -0,0 +1,13 @@
+<?php include('functions.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration system PHP and MySQL</title>
+</head>
+<body>
+	<div class="header">
+		<h2>No Flag for you!! Login as admin.</h2>
+	</div>
+</body>
+</html>
