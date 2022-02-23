5f0cdba3e9e480d48ad0aa440fc662c0809c0f41
itsmeaiswarya <69300687+itsmeaiswarya@users.noreply.github.com>
2022-02-23 20:49:39 +0530

    Create success.php

---
+++ BrokenAuthentication/success.php
@@ -0,0 +1,18 @@
+<?php include('functions.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration system PHP and MySQL</title>
+</head>
+<body>
+	<div class="header">
+		<h2>Welcome admin!!</h2> 
+
+	<?php
+	echo '<script>alert("Flag is br0k3n4dm1n4cc3ss3d")</script>';
+	?>
+
+	</div>
+</body>
+</html>
